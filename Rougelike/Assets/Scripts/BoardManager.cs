df821f99f4529b17dc9f4b8b301bb8e410233725
pysco <pahari76@gmail.com>
2026-04-20 01:24:21 +0545

    update

--- Rougelike/Assets/Scripts/BoardManager.cs
+++ Rougelike/Assets/Scripts/BoardManager.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    
-    }
+   private Tilemap m_Tilemap;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public int Width;
+   public int Height;
+   public Tile[] GroundTiles;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+       m_Tilemap = GetComponentInChildren<Tilemap>();
+
+       for (int y = 0; y < Height; ++y)
+       {
+           for(int x = 0; x < Width; ++x)
+           {
+               int tileNumber = Random.Range(0, GroundTiles.Length);
+               m_Tilemap.SetTile(new Vector3Int(x, y, 0), GroundTiles[tileNumber]);
+           }
+       }
+   }
+
 }
